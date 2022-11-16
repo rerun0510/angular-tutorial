36507e8d6455f3f357499c2bc6148dfeb6d025e0
Hiroki Kajiwara <rerun.0510@gmail.com>
2022-11-16 14:57:56 +0900

    section3-43

--- src/app/core/components/error-messaging/error-messaging.component.spec.ts
+++ src/app/core/components/error-messaging/error-messaging.component.spec.ts
@@ -1,22 +1,57 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TranslateTestingModule } from 'ngx-translate-testing';
+
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ErrorMessagingComponent } from './error-messaging.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ErrorMessagingService } from '../../services/error-messaging.service';
+import { By } from '@angular/platform-browser';
 
-xdescribe('ErrorMessagingComponent', () => {
+describe('ErrorMessagingComponent', () => {
   let component: ErrorMessagingComponent;
   let fixture: ComponentFixture<ErrorMessagingComponent>;
+  let errorMessagingServiceSpy: { clearMessageProperty: jasmine.Spy; getMessageProperty: jasmine.Spy };
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  beforeEach(async(() => {
+    errorMessagingServiceSpy = jasmine.createSpyObj('ErrorMessagingService', [
+      'clearMessageProperty',
+      'getMessageProperty'
+    ]);
+    TestBed.configureTestingModule({
+      schemas: [NO_ERRORS_SCHEMA],
+      imports: [TranslateTestingModule.withTranslations({ ja: require('src/assets/i18n/ja.json') })],
+      providers: [{ provide: ErrorMessagingService, useValue: errorMessagingServiceSpy }],
       declarations: [ErrorMessagingComponent]
     }).compileComponents();
+  }));
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(ErrorMessagingComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('#constructor', () => {
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+  });
+
+  describe('#ngOnInit', () => {
+    it('should init', () => {
+      component.ngOnInit();
+      expect(errorMessagingServiceSpy.clearMessageProperty.calls.count()).toBeGreaterThan(1);
+    });
+
+    it('should error display message', () => {
+      const errorMessageProperty = 'errMessage.http.badRequest';
+      const expectedValue = '入力情報が正しくありません。';
+
+      errorMessagingServiceSpy.getMessageProperty.and.returnValue(errorMessageProperty);
+      fixture.detectChanges();
+
+      const htmlParagraphElement: HTMLParagraphElement = fixture.debugElement.query(By.css('p')).nativeElement;
+      expect(htmlParagraphElement.innerText).toEqual(expectedValue);
+    });
   });
 });
