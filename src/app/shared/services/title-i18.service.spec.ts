41fe3f11f598c162f6023ef4f355d299f963f470
Hiroki Kajiwara <rerun.0510@gmail.com>
2022-11-16 15:04:25 +0900

    section3-45

--- src/app/shared/services/title-i18.service.spec.ts
+++ src/app/shared/services/title-i18.service.spec.ts
@@ -1,16 +1,32 @@
 import { TestBed } from '@angular/core/testing';
+import { TranslateTestingModule } from 'ngx-translate-testing';
 
 import { TitleI18Service } from './title-i18.service';
 
-xdescribe('TitleI18Service', () => {
+describe('TitleI18Service', () => {
   let service: TitleI18Service;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [TranslateTestingModule.withTranslations({ ja: require('src/assets/i18n/ja.json') })]
+    });
     service = TestBed.inject(TitleI18Service);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  describe('#constructor', () => {
+    it('should be created', () => {
+      expect(service).toBeTruthy();
+    });
+  });
+
+  describe('#setTitle', () => {
+    it('should set title', () => {
+      const screenName = 'sign-in';
+      const expectedTitleSystem = '【Example Site】';
+      const expectedTitleSub = 'ログイン';
+
+      service.setTitle(screenName);
+      expect(service.title.getTitle()).toEqual(expectedTitleSystem + expectedTitleSub);
+    });
   });
 });
