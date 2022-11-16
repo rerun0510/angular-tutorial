56fcab2d10be79a2a40784252b5b214a76d004bb
Hiroki Kajiwara <rerun.0510@gmail.com>
2022-11-16 12:52:45 +0900

    section3-36

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,10 +4,13 @@ import { LoadingComponent } from './components/loading/loading.component';
 import { ErrorMessagingComponent } from './components/error-messaging/error-messaging.component';
 import { MaterialModule } from '../material/material.module';
 import { NgxTranslateModule } from '../ngx-translate/ngx-translate.module';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { XhrInterceptor } from './interceptors/xhr.interceptor';
 
 @NgModule({
   declarations: [LoadingComponent, ErrorMessagingComponent],
   imports: [CommonModule, MaterialModule, NgxTranslateModule],
+  providers: [{ provide: HTTP_INTERCEPTORS, useClass: XhrInterceptor, multi: true }],
   exports: [LoadingComponent, ErrorMessagingComponent]
 })
 export class CoreModule {}
