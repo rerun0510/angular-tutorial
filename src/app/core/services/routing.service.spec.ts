50a4ce588248ad7036712e5b731b96c70dfc1cc5
Hiroki Kajiwara <rerun.0510@gmail.com>
2022-11-16 13:10:41 +0900

    session3-39

--- src/app/core/services/routing.service.spec.ts
+++ src/app/core/services/routing.service.spec.ts
@@ -1,16 +1,33 @@
 import { TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { UrlConst } from 'src/app/pages/constants/url-const';
 
 import { RoutingService } from './routing.service';
 
-xdescribe('RoutingService', () => {
+describe('RoutingService', () => {
   let service: RoutingService;
+  let router: Router;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule]
+    });
     service = TestBed.inject(RoutingService);
+    router = TestBed.inject(Router);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  describe('#constructor', () => {
+    it('should be created', () => {
+      expect(service).toBeTruthy();
+    });
+  });
+
+  describe('#navigate', () => {
+    it('should be navigated to sign in page', () => {
+      spyOn(router, 'navigate');
+      service.navigate(UrlConst.PATH_SIGN_IN);
+      expect(router.navigate).toHaveBeenCalledWith([UrlConst.SLASH + UrlConst.PATH_SIGN_IN]);
+    });
   });
 });
