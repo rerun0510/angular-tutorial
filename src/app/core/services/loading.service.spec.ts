70c94e6bb90cea5a9966140327a1b43859f2457b
Hiroki Kajiwara <rerun.0510@gmail.com>
2022-11-16 13:03:06 +0900

    section3-38

--- src/app/core/services/loading.service.spec.ts
+++ src/app/core/services/loading.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { LoadingService } from './loading.service';
 
-xdescribe('LoadingService', () => {
+describe('LoadingService', () => {
   let service: LoadingService;
 
   beforeEach(() => {
@@ -10,7 +10,23 @@ xdescribe('LoadingService', () => {
     service = TestBed.inject(LoadingService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  describe('#constructor', () => {
+    it('should be created', () => {
+      expect(service).toBeTruthy();
+    });
+  });
+
+  describe('#startLoading', () => {
+    it('should start loading', () => {
+      service.startLoading();
+      expect(service.isLoading).toBe(true);
+    });
+  });
+
+  describe('#stopLoading', () => {
+    it('should stop loading', () => {
+      service.stopLoading();
+      expect(service.isLoading).toBe(false);
+    });
   });
 });
